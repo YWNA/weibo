4e8949dcbcc4f9dfd01c80596f89581fb53d2776
chenbo <1264339504@qq.com>
2016-05-16 19:59:24 +0800

    ko

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -7,18 +7,16 @@ function closeMain(){
 $(function(){  
   function run() {
   $.post('/welcome/info',{cid:$('.weibo').attr('cid'),num:num}, function(data){
-    console.log(data.info.count);
+    if (num >= data.info.count) {num = 0;};
     if (data.info == null) {$('.marquee').html('<a style="margin-left: 10px;" href="#">客户ID不存在,请重新配</a>');return;};
     if (data.info.link) {
       var info='<a style="margin-left: 10px;" href="welcome/redirect/'+data.info.link+ '/' + data.info.id+'" target="_blank">'+data.info.title+'</a>';
     } else {
       var info='<a style="margin-left: 10px;" href="#" onclick="return false" target="_blank">'+data.info.title+'</a>';
     }
-    $('.marquee').html(info);
-    // $('.marquee').fadeIn();
-    // $('.marquee').fadeOut();
-    if (num >= data.info.count) {num = 0;console.log(data.info.count)};
-     num++;
+    $('.marquee').fadeOut('slow', function(){$('.marquee').html(info)});
+    $('.marquee').fadeIn('slow');
+    num++;
   })
 }
   num=1;
